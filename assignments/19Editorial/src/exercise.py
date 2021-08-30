3330702a9f7dfa40c13fe3025fdd52ef101c71e0
angelbolado <a01411879@itesm.mx>
2021-08-29 22:37:15 -0500

    Ejercicio 5

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,13 +1,11 @@
 def main():
     # escribe tu código abajo de esta línea
     pass
-
 import math
 num_palabras = int(input('Dame el número de palabras: '))
 num_paginas = math.ceil(num_palabras / 475)
 total = (num_paginas * 60)
 porcentaje = 90 * total / 100
-
 print('El costo de la publicación es:', porcentaje)
 
 if __name__ == '__main__':
