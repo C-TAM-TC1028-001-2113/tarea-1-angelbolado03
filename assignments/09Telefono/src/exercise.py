65f308e0d5303b23337755d1871aa2fc67b6f7df
angelbolado <a01411879@itesm.mx>
2021-08-30 09:52:10 -0500

    Ejercicio 2

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,10 +1,9 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
-mensajes = int(input('Dame el número de mensajes: '))
-megas = float(input('Dame el número de megas: '))
-minutos = int(input('Dame el número de minutos: '))
-costo = (mensajes + megas + minutos) * 0.8
+    mensajes = int(input('Dame el número de mensajes: '))
+    megas = float(input('Dame el número de megas: '))
+    minutos = int(input('Dame el número de minutos: '))
+    costo = (mensajes + megas + minutos) * 0.8
 
 print('El costo mensual es:', costo)
 
